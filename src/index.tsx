c17df94781f6c7fa893d2e1645b18b56b8bc3436
Kotaro <kotaro@KotaronoMacBook-Air.local>
2024-09-02 15:03:59 +0900

    recoilのインストール

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import './index.css';
+
+import { RecoilRoot } from 'recoil';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RecoilRoot>
+      <App />
+    </RecoilRoot>
   </React.StrictMode>
 );
 
